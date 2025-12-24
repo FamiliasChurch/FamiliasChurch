2e3763b46b7af737e91c01af1b9cbe8732eb072b
dagabriel31 <dagabriel31@gmail.com>
2025-12-24 05:32:48 -0300

    chore: configurando base path para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'serve' ? '/' : '/familias_church/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/Familias/', // Adicione exatamente o nome do seu repositório aqui entre barras
 })
